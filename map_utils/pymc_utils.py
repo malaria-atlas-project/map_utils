648b1cce39760905f6b9f9d54071a7a99c569f10
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-04-26 15:43:09 +0200

    Moved spatial submodel to utils

--- map_utils/pymc_utils.py
+++ map_utils/pymc_utils.py
@@ -1,7 +1,82 @@
 import pymc as pm
 import numpy as np
 
-__all__ = ['FieldStepper']
+__all__ = ['FieldStepper', 'combine_spatial_inputs','generic_spatial_submodel']
+
+def combine_spatial_inputs(lon,lat):
+    # Convert latitude and longitude from degrees to radians.
+    lon = lon*np.pi/180.
+    lat = lat*np.pi/180.
+    
+    # Make lon, lat tuples.
+    data_mesh = np.vstack((lon, lat)).T 
+    return data_mesh
+
+def basic_spatial_submodel(lon, lat, covariate_values, cpus):
+    logp_mesh = combine_spatial_inputs(lon,lat)
+
+    # =====================
+    # = Create PyMC model =
+    # =====================    
+    init_OK = False
+    while not init_OK:
+                
+        # Make coefficients for the covariates.
+        m_const = pm.Uninformative('m_const', value=0.)
+        
+        covariate_dict = {}
+        for cname, cval in covariate_values.iteritems():
+            this_coef = pm.Uninformative(cname + '_coef', value=0.)
+            covariate_dict[cname] = (this_coef, cval)
+
+        V = pm.Exponential('V',.1,value=1.)
+
+        inc = pm.CircVonMises('inc', 0,0)
+
+        @pm.stochastic(__class__ = pm.CircularStochastic, lo=0, hi=1)
+        def sqrt_ecc(value=.1):
+            return 0.
+        ecc = pm.Lambda('ecc', lambda s=sqrt_ecc: s**2)
+
+        amp = pm.Exponential('amp',.1,value=1.)
+
+        scale = pm.Exponential('scale',.1,value=1.)
+        
+        dd = pm.Uniform('dd',.5,3)
+            
+        # The mean of the field
+        @pm.deterministic(trace=True)
+        def M(mc=m_const):
+            return pm.gp.Mean(lambda x: mc*np.ones(x.shape[0]))
+        
+        # The mean, evaluated  at the observation points, plus the covariates    
+        @pm.deterministic(trace=False)
+        def M_eval(M=M, lpm=logp_mesh, cv=covariate_dict):
+            out = M(lpm)
+            for c in cv.itervalues():
+                out += c[0]*c[1]
+            return out
+
+        # Create covariance and MV-normal F if model is spatial.   
+        try:
+            # A Deterministic valued as a Covariance object. Uses covariance my_st, defined above. 
+            @pm.deterministic(trace=True)
+            def C(amp=amp,scale=scale,inc=inc,ecc=ecc):
+                return pm.gp.FullRankCovariance(pm.gp.cov_funs.matern.aniso_geo_rad, amp=amp, scale=scale, inc=inc, ecc=ecc, diff_degree=dd,n_threads=cpus)
+
+            # The evaluation of the Covariance object, plus the nugget.
+            @pm.deterministic(trace=False)
+            def C_eval(C=C):
+                return C(logp_mesh, logp_mesh)
+                                            
+            # The field evaluated at the uniquified data locations
+            f = pm.MvNormalCov('f',M_eval,C_eval)            
+            
+            init_OK = True
+        except pm.ZeroProbability, msg:
+            print 'Trying again: %s'%msg
+            init_OK = False
+            gc.collect()
 
 class FieldStepper(pm.StepMethod):
     """
