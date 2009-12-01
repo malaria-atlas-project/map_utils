cf13835022e4014f5ea88a42829c3b95af86cff0
PWG <peter.gething@zoo.ox.ac.uk>
2009-12-01 17:42:41 +0000

    trying to get checkAndBuildPaths to work

--- map_utils/__init__.py
+++ map_utils/__init__.py
@@ -2,11 +2,11 @@ import warnings
 import sys
 try:
     from templateHDF5_2_PixelAreaAscii import *
-    from checkAndBuildPaths import *
     from getAsciiheaderFromTemplateHDF5 import *    
 except:
     cls, inst, tb = sys.exc_info()
     print 'Failed to import a module. Error: ' + inst.message
+
 from quantile_funs import *
 from exportAscii import *
 from exportHDF5asASCII import *
@@ -20,6 +20,11 @@ from recarray_utils import *
 #from EmpiricalCovarianceLib import *
 
 from lazy_data_dir import *
+
+try:
+    from checkAndBuildPaths import *
+except:
+    print 'Failed to import checkAndBuildPaths'
 
 try:
     from modis_utils import *
